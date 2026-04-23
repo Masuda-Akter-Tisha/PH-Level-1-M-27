e10f16046203240591a3824c1fce801acda043ce
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-23 12:00:59 +0600

    updated file

--- primitive.js
+++ primitive.js
@@ -1,2 +1,15 @@
-// primitive 
+// primitive / basic / primary
 // string, numeric/number, boolean, null, undefined
+let data = 30;
+data = 'Tisha';
+data = false;
+data;
+data = null;
+
+
+// non-primitive / reference type
+data = {salary : 25000};
+data = [12, 45, 'flower'];
+data = (x) => x*x;
+
+console.log(typeof data);
