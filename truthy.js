53606070c10c64ee2845e76b7e47bddc7c8f69a6
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-26 20:34:32 +0600

    updated file

--- truthy.js
+++ truthy.js
@@ -1,4 +1,11 @@
 let data = 0;
+data = 1;
+data = '';
+data = undefined;
+data = null;
+data = '0';
+data = [];
+data = {};
 
 if (data) {
     console.log('truthy');
@@ -6,3 +13,17 @@ if (data) {
 else {
     console.log('falsy');
 }
+
+// logical not 
+let price = false;
+  if (!price) {  // jaita false shaita jenu if block a age dhoke
+    console.log('falsy');
+    
+ }
+
+//  double not
+let money = true;
+  if (!!money) {  // jaita false shaita jenu if block a age dhoke
+    console.log('truthy');
+    
+ }
