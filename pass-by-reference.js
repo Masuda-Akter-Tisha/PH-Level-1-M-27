22636d419248ff407f1bbf9b2ed9d2600cda6ce1
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-29 23:14:37 +0600

    added pass-by-reference.js

---
+++ pass-by-reference.js
@@ -0,0 +1,11 @@
+function array (array1, array2) {
+    array1[0] = 30;
+  console.log(array1, array2);
+}
+
+let x = [10, 20];
+let y = ['lily', 'sunflower'];
+console.log(x, y);
+
+ array (x, y);
+ console.log(x, y);
