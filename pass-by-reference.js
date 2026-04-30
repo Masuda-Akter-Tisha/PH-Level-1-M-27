3d0cd3b4fdf61f7d0082a6aeb504d55d4783a7ed
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-04-30 21:17:40 +0600

    added pass-by-reference.js file and updated file

--- pass-by-reference.js
+++ pass-by-reference.js
@@ -1,11 +1,32 @@
-function array (array1, array2) {
-    array1[0] = 30;
-  console.log(array1, array2);
+function firstSum (array1, array2) {
+  array1[0] = 100;
+  array2[0] = 200;
+  let first = array1[0];
+  let second = array2[0];
+   return first + second; 
 }
+ 
+let num1 = [1, 2, 3];
+let num2 = [4, 5, 6];
 
-let x = [10, 20];
-let y = ['lily', 'sunflower'];
-console.log(x, y);
+console.log('befor calling function', num1, num2);
 
- array (x, y);
- console.log(x, y);
+let result = firstSum (num1, num2);
+console.log(result);
+
+console.log('after calling function', num1, num2);
+
+
+// function array (array1, array2) {
+//     array1[0] = 30;
+//   console.log(array1, array2);
+// }
+
+// let x = [10, 20];
+// let y = ['lily', 'sunflower'];
+// console.log(x, y);
+
+//  array (x, y);
+//  console.log(x, y);
+
+
