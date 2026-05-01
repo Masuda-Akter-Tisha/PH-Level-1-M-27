75c158118c0c9baa4758036a919767c958f45572
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-01 23:08:07 +0600

    added forEach.js file

---
+++ forEach.js
@@ -0,0 +1,9 @@
+const products = [
+    {name: 'iphone', price: 12000},
+    {name: 'tablet', price: 25000},
+    {name: 'intel', price: 2000},
+]
+
+products.forEach (num => console.log(num.price));
+
+// forEach does not return anything.
