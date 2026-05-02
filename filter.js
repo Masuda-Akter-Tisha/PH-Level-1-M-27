c7b9b7ab0d5a83e6ec29f018b12b7ed0678edb85
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-02 10:26:55 +0600

    updated file

--- filter.js
+++ filter.js
@@ -2,9 +2,22 @@ const numbers = [10, 33, 44, 60];
 const evenNumber = numbers.filter (num => num % 2 === 0);
 console.log(evenNumber);
 
+const friends = ['maria', 'khadija', 'oafa', 'halima'];
+// const frnd = friends.filter (fd => fd.length === 5);
+const frnd = friends.filter (fd => fd[1] === 'a');
+console.log(frnd);
 
-let x = 5; 
-let y = x++;
+const students = [
+    {name:'halima', age: 23},
+    {name:'shima', age: 33},
+    {name:'sana', age: 20},
+    {name:'sheza', age: 22},
+    {name:'oafa', age: 15},
+    {name:'maria', age: 39},
+    {name:'sehnaz', age: 40}
+]
 
-console.log(x, y);
+const student = students.filter (std => std.age > 25 && std.name[0] === 's');
+console.log(student);
+
 
