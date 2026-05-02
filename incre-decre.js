74d7f9d250ead602e37c51771fe498ae8e8e7b3c
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-02 20:30:21 +0600

    added incre-decre.js file

---
+++ incre-decre.js
@@ -0,0 +1,33 @@
+// let a = 5;
+// a ++;
+// // ++ a;
+
+// console.log(a ++);
+
+// console.log(a);
+
+//  post-increment
+let x = 5;
+ let y = x ++;
+
+ console.log(x, y);
+
+//  pre-increment
+ let p = 5;
+ let q = ++ p;
+
+ console.log(p, q);
+ 
+ //  post-decrement
+ let s = 10;
+ let t = s --;
+
+ console.log(s, t);
+ 
+//  pre-increment
+ let d = 10;
+ let e = -- d;
+
+ console.log(d, e);
+ 
+ 
